3c850032dd34a1083265f947391d2e6dc5d3f4a9
CiucurDaniel <ciucur.daniel14@gmail.com>
2019-10-28 21:20:59 +0200

    lab1 exercises

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,7 +1,22 @@
 package lab1;
 
 public class Main {
+    void fizzBuzz(){
+        for(int i = 1 ; i <= 100; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                System.out.println("FizzBuzz");
+            } else if(i % 3 ==0 ){
+                System.out.println("Fizz");
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
+        }
+    }
     public static void main(String[] args){
+        Main a = new Main();
+        a.fizzBuzz();
         System.out.println("Atelierul digital 2019");
     }
 }
